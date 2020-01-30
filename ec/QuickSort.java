3f70de1a7057aef57d1c1edc54cdd394eb1be969
Sudarshan Guttula <sidguttula@yahoo.com>
2020-01-29 16:31:39 -0800

    Swaps and comparisons updated

--- ec/QuickSort.java
+++ ec/QuickSort.java
@@ -10,7 +10,7 @@ public class QuickSort<T extends Comparable<T>> implements SortingAlgorithms{
 	static long start;
 	static long end;
 	public QuickSort() {
-		// TODO Auto-generated constructor stub
+		
 	}
 	public int quickSort1(ArrayList<T> quick, int i, int j) {
 		T turn = quick.get(j);
@@ -31,24 +31,16 @@ public class QuickSort<T extends Comparable<T>> implements SortingAlgorithms{
 		T p = quick.get(bottom + 1);
 		quick.set(bottom + 1, quick.get(j));
 		quick.set(j, p);
-//		methodToTime();
 		return bottom + 1;
 	}
 	public ArrayList<T> quickSort2(ArrayList<T> list2) {
-//		System.out.println("Comparison: " + compareQuick);
-//		System.out.println("Swaps: " + swapsQuick);
-//		System.out.println("Runtime: " + runtime + " milliseconds");
 		return quickSort3(list2, 0, list2.size() - 1);
 	} 
 	public ArrayList<T> quickSort3(ArrayList<T> quick, int i, int j) {
 		if(i < j) {
-			//start = System.currentTimeMillis();
 			int sort = quickSort1(quick, i, j);
 			quickSort3(quick, i, sort - 1);
 			quickSort3(quick, sort + 1, j);
-//			end = System.currentTimeMillis();
-//			runtime = end - start;
-
 		}
 		
 		return quick;
@@ -84,38 +76,4 @@ public class QuickSort<T extends Comparable<T>> implements SortingAlgorithms{
 	public void runtime() {
 		System.out.print("Runtime: "  + runtime + " ms");
 	}
-//	static ArrayList<Long> quickSort2(ArrayList<Long> arrayListInput) {
-//		return quickSort(arrayListInput, 0, arrayListInput.size() - 1);
-//
-//	}
-//
-//	private static ArrayList<Long> quickSort(ArrayList<Long> list, int low, int high) {
-//		if (low < high) {
-//			int p = partition(list, low, high);
-//			quickSort(list, low, p - 1);
-//			quickSort(list, p + 1, high);
-//		}
-//		return list;
-//	}
-//
-//	private static int partition(ArrayList<Long> arrayListInput, int low, int high) {
-//		long pivot = arrayListInput.get(high);
-//		int i = low - 1;
-//		for (int j = low; j <= high - 1; j++) {
-//			//this.numComparisons++;
-//			if (arrayListInput.get(j).compareTo(pivot) <= 0) {
-//				i = i + 1;
-//				//this.numSwaps++;
-//				long temp = arrayListInput.get(i);
-//				arrayListInput.set(i, arrayListInput.get(j));
-//				arrayListInput.set(j, temp);
-//			}
-//		}
-//		//this.numSwaps++;
-//		long temp = arrayListInput.get(i + 1);
-//		arrayListInput.set(i + 1, arrayListInput.get(high));
-//		arrayListInput.set(high, temp);
-//		
-//		return i + 1;
-//	}
 }
